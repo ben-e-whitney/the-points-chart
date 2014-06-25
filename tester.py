39972013fef0ce31156add48182947cc6a0d736d
Ben E. Whitney <ben.e.whitney@post.harvard.edu>
2014-06-25 19:43:10 -0400

    Add .gitignore and remove files produced by Python.

--- tester.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import User
-import chores.views
-
-indy = User.objects.all()[1]
-list_obl = chores.views.get_obligations(indy)['list_information']
-signed_up = list_obl[0]['sections'][0]
