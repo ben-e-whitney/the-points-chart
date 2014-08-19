753988b3b37baeef969032c9f3bfde8c735d2188
Ben E. Whitney <ben.e.whitney@post.harvard.edu>
2014-08-19 19:24:56 -0400

    Improve formatting of stats page.

---
+++ chores/static/chores/user_stats_list.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $('.date_picker').datepicker();
+  $('#form_accordion').accordion({active: 0, collapsible: true,
+    heightStyle: "content", animate: false});
+});
