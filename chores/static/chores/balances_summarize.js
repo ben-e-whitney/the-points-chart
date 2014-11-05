a1d9a442e99c804f7a0ed746579257efc86abfb0
Ben E. Whitney <ben.e.whitney@post.harvard.edu>
2014-11-05 18:22:39 -0500

    Make `columnize` function available to all pages.

--- chores/static/chores/balances_summarize.js
+++ chores/static/chores/balances_summarize.js
@@ -1,38 +1,3 @@
-var columnize = function($element) {
-  var spacer_width = 1*parseFloat($element.css('font-size'), 10);
-  var main_content_width = $('#main_content').width();
-  var cell_width = $element.find('tr').width();
-  var num_columns = Math.floor(main_content_width/(cell_width+spacer_width));
-  spacer_width = String(spacer_width);
-  if (num_columns == 1) {
-    return;
-  }
-  var entry_htmls = []
-  $.each($element.find('tr'), function(index, row) {
-    $row = $(row);
-    entry_htmls.push($row.html());
-    $row.remove();
-  });
-  var column;
-  var table_html = ''
-  $.each(entry_htmls, function(index, entry_html) {
-    column = index % num_columns;
-    if (column == 0) {
-      entry_html = '<tr>'+entry_html;
-    }
-    if (column == num_columns-1) {
-      entry_html = entry_html+'</tr>';
-    } else {
-      entry_html = entry_html+'<td style="width: '+spacer_width+'"></td>';
-    }
-    table_html += entry_html;
-  });
-  if (column != num_columns-1) {
-    table_html += '</tr>';
-  }
-  $element.append(table_html);
-};
-
 $(window).load(function() {
   columnize($('#balances_table'));
 });
