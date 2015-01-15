5f83fbbfe4cfc8bcf11404a76942275fc7e2382c
Ben E. Whitney <ben.e.whitney@post.harvard.edu>
2015-01-15 16:21:18 -0500

    Rename files and redirect / to /about/.

--- the_points_chart/urls.py
+++ the_points_chart/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', RedirectView.as_view(url='/chores/')),
+    url(r'^$', RedirectView.as_view(url='/about/')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^chores/', include('chores.urls')),
     url(r'^steward/', include('steward.urls')),
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
     url(r'^login/', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/', 'django.contrib.auth.views.logout',
         {'next_page': '/login/'}, name='logout'),
-    url(r'^summary/', 'chores.views.balances_summarize',
+    url(r'^balances/', 'chores.views.balances_summarize',
         name='balances_summarize'),
     url(r'^about/', 'utilities.views.about', name='about'),
 )
