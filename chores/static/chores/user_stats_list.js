1f6d61f53dd192dfd5e37fc20ad3537af1668a3a
Ben E. Whitney <ben.e.whitney@post.harvard.edu>
2015-01-15 16:35:04 -0500

    Tweak display.

--- chores/static/chores/user_stats_list.js
+++ chores/static/chores/user_stats_list.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $('.date_picker').datepicker();
-  $('#form_accordion').accordion({active: 0, collapsible: true,
+  $('#form_accordion').accordion({active: 1, collapsible: true,
     heightStyle: "content", animate: false});
 });
