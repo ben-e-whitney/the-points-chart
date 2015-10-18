ab3bc85c87a0db36ae2b75d28544b7af94230ea2
Ben E. Whitney <ben.e.whitney@post.harvard.edu>
2015-10-18 15:43:12 -0400

    Restore ability of steward to void others' chores.

--- chores/models.py
+++ chores/models.py
@@ -276,7 +276,7 @@ class Timecard(models.Model):
     @actor_commit_wrap
     def void(self, user, *args, **kwargs):
         testing = kwargs.pop('testing', False)
-        if self.signed_up.who != user:
+        if self.signed_up.who != user and not user.profile.points_steward:
             raise ChoreError("You can't void a chore that isn't yours.")
         if self.in_the_future(user.profile.coop):
             raise ChoreError("You can't void a chore before it has been done.")
